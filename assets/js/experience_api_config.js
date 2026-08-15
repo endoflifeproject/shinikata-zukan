c95c1b68c627411c7fdc4e9042898e2ea57fdb04
endoflifeproject <endoflife.project.jp@gmail.com>
2026-08-16 01:46:59 +0900

    Enable local expert review API submission

--- assets/js/experience_api_config.js
+++ assets/js/experience_api_config.js
@@ -4,6 +4,7 @@
   window.SHINIKATA_API_CONFIG = Object.freeze({
     baseUrl: localApiRequested ? 'http://127.0.0.1:8787' : '',
     enableSubmission: localApiRequested,
+    enableReviewSubmission: localApiRequested,
     enablePublicStats: localApiRequested,
     consentVersion: localApiRequested ? 'local-dev-only-2026-08-16' : 'not-enabled',
     requiredConsentScope: 'research_aggregate_use'
