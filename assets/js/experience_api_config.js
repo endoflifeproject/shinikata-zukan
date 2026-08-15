1ea9237d68d5ed628eefeeb1bdab3ea98b643af2
endoflifeproject <endoflife.project.jp@gmail.com>
2026-08-16 01:08:57 +0900

    Add safe API client configuration

---
+++ assets/js/experience_api_config.js
@@ -0,0 +1,11 @@
+(function(){
+  const local = location.hostname === '127.0.0.1' || location.hostname === 'localhost';
+  const localApiRequested = local && new URLSearchParams(location.search).get('api') === 'local';
+  window.SHINIKATA_API_CONFIG = Object.freeze({
+    baseUrl: localApiRequested ? 'http://127.0.0.1:8787' : '',
+    enableSubmission: localApiRequested,
+    enablePublicStats: localApiRequested,
+    consentVersion: localApiRequested ? 'local-dev-only-2026-08-16' : 'not-enabled',
+    requiredConsentScope: 'research_aggregate_use'
+  });
+})();
